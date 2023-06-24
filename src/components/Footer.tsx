30fe8d15b2cc18ac89da46699da4623d8baa1b6a
Aimen <hdmen55@hotmail.com>
2023-06-24 16:43:27 -0400

    fix logo, header and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: FC = () => {
   return (
     <footer className="fixed bottom-0 left-0 right-0 flex items-center justify-between px-4 py-2 bg-blue-900 text-white">
       <a href="/">
-        <img src="/logo.png" alt="Logo" className="h-8 mr-3" />
+        <img src="/images/logo.png" alt="Logo" className="h-16 mr-3" />
       </a>
       <nav className="flex items-center justify-center gap-4">
         <a href="/marketplace" className="text-white hover:underline">
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,36 +22,36 @@ const Header: FC<HeaderProps> = ({ onSearch }) => {
   const unreadNotifications =
     notifications?.filter((notification) => !notification.read) || [];
 
-  return (
-    <header className="flex justify-between px-4 py-2 bg-blue-900 text-white items-center">
-      <nav className="flex gap-4">
-        <Link href="/">
-          <span className="text-3xl font-bold text-white cursor-pointer">
-            MedChain
-          </span>
-        </Link>
-        <Link href="/publish">
-          <span className="text-lg text-white cursor-pointer">
-            Publish Data
-          </span>
-        </Link>
-        <Link href="/account">
-          <span className="text-lg text-white cursor-pointer">Account</span>
-        </Link>
-      </nav>
-      <div className="flex gap-4">
-        <SearchBar onSearch={onSearch} />
-        <div>
-          <span className="relative inline-block cursor-pointer">
-            <NotificationDropdown />
-          </span>
+    return (
+      <header className="flex justify-between px-4 py-2 bg-blue-900 text-white items-center">
+        <nav className="flex gap-4">
+          <Link href="/">
+            <span className="text-3xl font-bold text-white cursor-pointer">
+              MedChain
+            </span>
+          </Link>
+          <Link href="/publish">
+            <span className="text-lg text-white cursor-pointer">
+              Publish Data
+            </span>
+          </Link>
+        </nav>
+        <div className="flex gap-4">
+          <SearchBar onSearch={onSearch} />
+          <div>
+            <span className="relative inline-block cursor-pointer">
+              <NotificationDropdown />
+            </span>
+          </div>
+          <Link href="/account">
+            <button className="px-6 py-4 bg-blue-700 text-white rounded cursor-pointer">
+              Account
+            </button>
+          </Link>
         </div>
-        <button className="px-4 py-2 bg-blue-700 text-white rounded">
-          Connect Wallet
-        </button>
-      </div>
-    </header>
-  );
+      </header>
+    );
+    
 };
 
 export default Header;
