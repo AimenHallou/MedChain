eac4adecd7c44c364e5c5362504e2fe7e2e4e3a4
Aimen <hdmen55@hotmail.com>
2023-10-26 04:46:49 -0400

    frontend setup

--- src/components/homepage/HomePage.tsx
+++ src/components/homepage/HomePage.tsx
@@ -10,11 +10,11 @@ interface HomePageProps {
 
 const HomePage: FC<HomePageProps> = ({ searchTerm }) => {
   return (
-    <div className="bg-page-background text-white min-h-screen py-8 px-6">
+    <div className="bg-gradient-animate animate-gradient-shift bg-200% text-white min-h-screen py-8 px-6">
       <div className="text-center max-w-2xl mx-auto">
         <h1 className="text-4xl py-1 text-white">Welcome to MedChain</h1>
         <p className="text-sm font-semibold text-gray-400">
-        Welcome to MedChain: A secure platform for patient data collaboration.
+          Welcome to MedChain: A secure platform for patient data collaboration.
         </p>
       </div>
       <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-10 gap-6 mt-8">
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,34 @@ module.exports = {
   content: ["./src/**/*.{html,ts,tsx}"],
   theme: {
     extend: {
+      backgroundImage: {
+        'gradient-animate': 'linear-gradient(to right, #001f40, #002855, #003366, #002855, #001f40)',
+      },
+      animation: {
+        'gradient-shift': 'gradientShift 15s infinite linear',
+      },
+      keyframes: {
+        gradientShift: {
+          '0%': {
+            'background-position': '100% 0',
+          },
+          '100%': {
+            'background-position': '0 0',
+          },
+        },
+      },
+      backgroundSize: {
+        '200%': '100%',
+      },
       backgroundColor: {
         "page-background": "#002855",
-        "page-header": "#538FF8",
+        "page-header-dark": "#001230",
         "page-button": "#A2ACBD",
         "page-hover-button": "#468faf",
-        "module-background": "#1A344B",
-        "module-accent": "#3498DB",
+        "module-background": "#001F52",
+      },
+      borderColor: {
+        "module-accent": "#000000",
       },
       textColor: {
         "main-text": "#E1E8ED",
