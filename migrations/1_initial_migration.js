48cf86102cea5927254b202d76bec4fac95d6046
Aimen <hdmen55@hotmail.com>
2023-11-21 13:29:13 -0500

    setup automatted contract update

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,8 +1,23 @@
 // migrations/1_initial_migration.js
+const fs = require('fs');
+const path = require('path');
 const PatientRegistry = artifacts.require("PatientRegistry");
 const DatasetRegistry = artifacts.require("DatasetRegistry");
 
 module.exports = function (deployer) {
-  deployer.deploy(PatientRegistry);
+  deployer.deploy(PatientRegistry).then(() => {
+    const configPath = path.join(__dirname, '..', 'config', 'development.js');
+    fs.readFile(configPath, 'utf8', function (err, data) {
+      if (err) {
+        return console.log(err);
+      }
+      var result = data.replace(/patientRegistryContract: ".*"/, `patientRegistryContract: "${PatientRegistry.address}"`);
+
+      fs.writeFile(configPath, result, 'utf8', function (err) {
+         if (err) return console.log(err);
+      });
+    });
+  });
+
   deployer.deploy(DatasetRegistry);
 };
