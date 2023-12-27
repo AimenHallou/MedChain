697f2e8343ca4310b70533e39d05e546e1c85f2d
Aimen <hdmen55@hotmail.com>
2023-12-27 11:04:05 -0500

    added production config

---
+++ config/production.js
@@ -0,0 +1,5 @@
+// config/production.js
+module.exports = {
+    patientRegistryContract: "0xD3e2b1F02298A8072377f20B30dDfFF8F4bF31ad",
+    AES_ENCRYPTION_KEY: "83bd6987d6729df1f47aaf49af44dab61a808bfefe8137378119409628b67142"
+  };
