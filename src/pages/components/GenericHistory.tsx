00ca6551fdac4d8000876e679d178ede4fc6415a
Aimen <hdmen55@hotmail.com>
2023-12-27 11:13:20 -0500

    Fix parsing error in GenericHistory component

--- src/pages/components/GenericHistory.tsx
+++ src/pages/components/GenericHistory.tsx
@@ -3,20 +3,28 @@ import React, { FC } from "react";
 
 interface HistoryProps {
   title: string;
-  data: string[];
+  data: string[] | string;
 }
 
 const GenericHistory: FC<HistoryProps> = ({ title, data }) => {
-console.log(typeof(data))
-console.log("here is the history",data)
-let parsedHistory = Array.isArray(data) ? history : JSON.parse(data);
+  let parsedHistory;
+  if (typeof data === 'string') {
+    try {
+      parsedHistory = JSON.parse(data);
+    } catch (error) {
+      console.error("Parsing error in GenericHistory", error);
+      parsedHistory = [];
+    }
+  } else {
+    parsedHistory = data;
+  }
 
   return (
     <div className="px-4 py-2 bg-gray-700 rounded-lg">
       <div className="border-t-2 border-dashed border-white py-2">
         <h2 className="text-xl font-bold text-white">{title}</h2>
       </div>
-      {parsedHistory.map((entry, index) => (
+      {Array.isArray(parsedHistory) && parsedHistory.map((entry, index) => (
         <div
           key={index}
           className="text-sm text-gray-200 bg-gray-800 p-3 my-2 rounded-md shadow-lg"
